186b96bcf080a9c70abbd611fc34b76de5f30697
Natanael04 <48935528+Natanael04@users.noreply.github.com>
2020-05-15 17:59:35 -0400

    intento de scanner

--- resources/views/Cliente/Escanear.blade.php
+++ resources/views/Cliente/Escanear.blade.php
@@ -2,4 +2,96 @@
 
 @section('content')
 <h1>pagina del Cliente </h1>
+<style type="text/css">
+                .scanner-laser{
+                        position: absolute;
+                        margin: 40px;
+                        height: 30px;
+                        width: 30px;
+                }
+                .laser-leftTop{
+                        top: 0;
+                        left: 0;
+                        border-top: solid red 5px;
+                        border-left: solid red 5px; 
+                }
+                .laser-leftBottom{
+                        bottom: 0;
+                        left: 0;
+                        border-bottom: solid red 5px;
+                        border-left: solid red 5px; 
+                }
+                .laser-rightTop{
+                        top: 0;
+                        right: 0;
+                        border-top: solid red 5px;
+                        border-right: solid red 5px;    
+                }
+                .laser-rightBottom{
+                        bottom: 0;
+                        right: 0;
+                        border-bottom: solid red 5px;
+                        border-right: solid red 5px;    
+                }
+        </style>
+         <div style="position: relative;display: inline-block;">
+            <canvas id="qr-canvas" width="320" height="240"></canvas>    
+            <div class="scanner-laser laser-rightBottom" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-rightTop" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-leftBottom" style="opacity: 0.5;"></div>
+            <div class="scanner-laser laser-leftTop" style="opacity: 0.5;"></div>
+        </div>
+        
+        <script src="/qr/jquery.js"></script>
+        <script src="/qr/WebCodeCam.js"></script>
+        <script src="/qr/qrcodelib.js"></script>
+        <script src="/qr/DecoderWorker.js"></script>
+
+    
+    <script type="text/javascript">
+
+        $('#qr-canvas').WebCodeCam({
+            ReadQRCode: true, // false or true
+            ReadBarecode: true, // false or true
+            width: 320,
+            height: 240,
+            videoSource: {  
+                    id: true,      //default Videosource
+                    maxWidth: 640, //max Videosource resolution width
+                    maxHeight: 480 //max Videosource resolution height
+            },
+            flipVertical: false,  // false or true
+            flipHorizontal: false,  // false or true
+            zoom: -1, // if zoom = -1, auto zoom for optimal resolution else int
+            beep: "/qr/beep.mp3", // string, audio file location
+            autoBrightnessValue: false, // functional when value autoBrightnessValue is int
+            brightness: 0, // int 
+            grayScale: false, // false or true
+            contrast: 0, // int 
+            threshold: 0, // int 
+            sharpness: [], //or matrix, example for sharpness ->  [0, -1, 0, -1, 5, -1, 0, -1, 0]
+            resultFunction: function(resText, lastImageSrc) {
+                        /* resText as decoded code, lastImageSrc as image source
+                        example:
+                        alert(resText);
+                        */ 
+            },
+            getUserMediaError: function() {
+                        /* callback funtion to getUserMediaError
+                        example:
+                        alert('Sorry, the browser you are using doesn\'t support getUserMedia');
+                        */
+            },
+            cameraError: function(error) {
+                        /* callback funtion to cameraError, 
+                        example:
+                        var p, message = 'Error detected with the following parameters:\n';
+                        for (p in error) {
+                                message += p + ': ' + error[p] + '\n';
+                        }
+                        alert(message);
+                        */
+            }
+        });
+    </script>
 @endsection
